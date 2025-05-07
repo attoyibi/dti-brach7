7e6c7473ff6e27181ec6ea87b593fdb1b22cc28c
Anisa Febriyani17 <anisa.febriyani17@gmail.com>
2025-05-08 01:20:33 +0700

    add: create function, method and Props

--- week4/react-anisa/react-nisa/src/Header.jsx
+++ week4/react-anisa/react-nisa/src/Header.jsx
@@ -1,9 +1,11 @@
-import  './assets/css/header.css'
+import "./assets/css/header.css";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <div>
-      <h1 className='background-blue header-container'>Header</h1>
+      <h1 className="background-blue header-container">Header {props.name}</h1>
+      <h1 className="background-blue header-container">Umur {props.age}</h1>
+      <h1 className="background-blue header-container">Ini Buah {props.listFruit[0]}</h1>
     </div>
-  )
+  );
 }
