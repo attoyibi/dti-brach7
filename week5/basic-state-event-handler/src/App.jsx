9a71e9247faa04490f02e9e64f709a47e8c98b51
attoyibi <43211768+attoyibi@users.noreply.github.com>
2025-05-12 20:28:57 +0700

    add : state and event handler basic

--- week5/basic-state-event-handler/src/App.jsx
+++ week5/basic-state-event-handler/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// tempat import inisialisasi
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  // tempat logic
+  let name = "rizky";// variabel biasa 
+  // buat dalam bentuk state
+  let varAge = 25
+  const [age, setAge] = useState(25); // dalam bentuk state
 
+  //event handler
+  function handleClick() {
+    console.log("handle click")
+    setAge(varAge + 1);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // tempat ditampikan
+    <div>
+      <p>name : {name}</p>
+      <p>Age : {age}</p>
+      <p>varAge : {varAge}</p>
+      <button type="button" onClick={handleClick}>ganti</button>
+    </div>
   )
 }
-
-export default App
