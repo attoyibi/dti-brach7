24efb181140a133b2e94271aa954ec411ff45cdf
RizkyKiky <samrizky345@gmail.com>
2025-05-20 21:11:18 +0700

    add : alternatif logic

--- week6/hook-fetch-data/src/App.jsx
+++ week6/hook-fetch-data/src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function App() {
+  // const [title, setTitle] = useState();
+  const [form, setForm] = useState({
+    title: '',
+    price: '',
+    category: '',
+  });
+
+  function handleChange(e) {
+    const { value, name } = e.target;
+    // console.log(name + ':' + value);
+    const updateForm = { ...form };
+    updateForm[name] = value;
+    setForm(updateForm);
+    console.log(form);
+  }
   return (
-    <div>App</div>
-  )
+    <div>
+      <form action="">
+        <input onChange={handleChange} name="title" placeholder="Title"></input>
+        <input onChange={handleChange} name="price" placeholder="Price"></input>
+        <input onChange={handleChange} name="category" placeholder="Category"></input>
+      </form>
+    </div>
+  );
 }
