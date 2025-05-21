fa70cac943cf7cd3a9afcf43134bfe5f7fa5539c
attoyibi <43211768+attoyibi@users.noreply.github.com>
2025-05-21 21:24:51 +0700

    add : create get fetching data products

--- week6/hook-fetch-data/src/BodyLeft.jsx
+++ week6/hook-fetch-data/src/BodyLeft.jsx
@@ -1,26 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import Timer from './Timer';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
 export default function BodyLeft() {
     const [name, setName] = useState();
     const [age, setAge] = useState();
     const [isShow, setIsShow] = useState(true);
     const [disabled, setDisbaled] = useState(true);
+    const [products, setProducts] = useState([]);
 
     // selalu berjalan
     useEffect(() => {
-        //disinilah tempat loginya nanti
+        //disinilah tempat logikanya nanti
         console.log("ini use effect selalu berjalan")
     });
 
     // tahap mounting
     useEffect(() => {
-        //disinilah tempat loginya nanti
+        //disinilah tempat logikanya nanti
+        // biasanya menjadi tempat fetching data
+        axios.get("https://dummyjson.com/products").then((respond) => {
+            //tempat logic ketika berhasil
+            console.log(respond.data.products);
+            setProducts(respond.data.products);
+        }).catch(
+            // tempat logic ketaka gagal
+
+        );
+
         console.log("ini use effect tahap mounting")
     }, []);
 
     // tahap updating
     useEffect(() => {
-        //disinilah tempat loginya nanti
+        //disinilah tempat logikanya nanti
         console.log("tahap updating name")
         if (name == "muchson") {
             setDisbaled(false);
@@ -52,7 +64,21 @@ export default function BodyLeft() {
                 <button type="button" disabled={disabled}>Submit</button>
             </div>
             <button type="button" onClick={handleClick} >Hidden/Show</button>
-            {isShow && <Timer />}
+            {/* {isShow && <Timer />} */}
+            <hr />
+            {/* Ini Product Title : {products.map} */}
+            List Product : {products.map((product, index) => (
+                <>
+                    <div style={{ border: 'solid 2px gray', margin: '2px' }}>
+                        <div >
+                            Product Title :   {product.title}
+                        </div>
+                        <div>
+                            Description :   {product.description}
+                        </div>
+                    </div>
+                </>
+            ))}
         </div>
     )
 }
