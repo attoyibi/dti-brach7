0c3c9ac771a4eafac582fd718ae7a261b37eeedf
attoyibi <43211768+attoyibi@users.noreply.github.com>
2025-05-28 21:11:42 +0700

    add : basic route 2

---
+++ week 7/routing-fetch-lanjutan/basic-route2/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import Products from './pages/ProductPage'
+export default function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        <div>
+          <Link to="/product/1">Product 1</Link> |
+          <Link to="/product/2">Product 2</Link> |
+          <Link to="/product/3">Product 3</Link> |
+          <Link to="/product/4">Product 4</Link> |
+          <Link to="/product/5">Product 5</Link> |
+        </div>
+      </nav>
+      {/* yang berganti hanya di dalam routes */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="/product/:id" element={<Products />} />
+      </Routes>
+      <footer>ini footer tidak berganti</footer>
+    </div>
+  )
+}
---
+++ week 7/routing-fetch-lanjutan/basic-route2/src/pages/ProductPage.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from "react-router";
+import axios from 'axios';
+export default function Products() {
+    let { id } = useParams();
+    const [products, setProducts] = useState([]);
+    console.log(id);
+    // tahap mounting
+    useEffect(() => {
+        //disinilah tempat logikanya nanti
+        // biasanya menjadi tempat fetching data
+        axios.get("https://dummyjson.com/products/" + id).then((respond) => {
+            //tempat logic ketika berhasil
+            console.log(respond.data);
+            setProducts([respond.data]);
+        }).catch(
+            // tempat logic ketaka gagal
+        );
+        console.log("ini use effect tahap mounting")
+    }, [id]);
+    return (
+        <div>Products -
+            List Product : {products.map((product, index) => (
+                <>
+                    <div style={{ border: 'solid 2px gray', margin: '2px' }}>
+                        <div >
+                            Product Title :   {product.title}
+                        </div>
+                        <div>
+                            Description :   {product.description}
+                        </div>
+                    </div>
+                </>
+            ))}
+        </div>
+    )
+}
